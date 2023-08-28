a402d2fc50b4bded74f5aebbaa463ac03c105aee
Issawara <kmind46@gmail.com>
2023-08-28 14:35:47 +0700

    1

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,26 @@
+"use client";
+
+import { runningPlans } from "@/libs/runningPlans";
+import {
+  Button,
+  Container,
+  Group,
+  Select,
+  TextInput,
+  Title,
+} from "@mantine/core";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <Container size="xs">
+      <Title italic color="violet">
+        Register CMU Marathon
+      </Title>
+      <Group grow>
+        <TextInput label="First Name" placeholder="Input First Name" />
+        <TextInput label="Last Name" placeholder="Input Last Name" />
+      </Group>
+      <Select data={runningPlans} label="Plan" placeholder="Select a Plan..." />
+    </Container>
+  );
 }
